d013e4eca61544722b56334f55ca558a8769cdf3
Julio <julbetan@gmail.com>
2024-12-05 15:38:04 -0600

    Agregar llamados API_CLIMA

--- app/routes/events.py
+++ app/routes/events.py
@@ -89,28 +89,70 @@ def convert_to_decimal(dms_str: str) -> tuple:
         print(f"Error al convertir coordenadas: {e}")
         return None
 
-
-async def get_weather_by_coordinates(coordinates: str, date: str) -> dict:
-   
-    latitude, longitude = convert_to_decimal(coordinates)
-
+# Función para obtener el clima
+async def get_weather(city: str, date: str) -> dict:
     api_key = WEATHER_KEY
-    url = f"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
+    url = f"http://api_clima:3000/clima?city={city}"
+    # url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
 
     async with httpx.AsyncClient() as client:
         try:
             response = await client.get(url)
             response.raise_for_status()
             weather_data = response.json()
-            return {
-                "temperature": weather_data["main"]["temp"],
-                "wind_speed": weather_data["wind"]["speed"],
-                "description": weather_data["weather"][0]["description"],
-            }
+            # Verificar si la estructura esperada está presente
+            if "main" in weather_data and "wind" in weather_data and "weather" in weather_data:
+                return {
+                    "temperature": weather_data["main"]["temp"],
+                    "wind_speed": weather_data["wind"]["speed"],
+                    "description": weather_data["weather"][0]["description"],
+                }
+            else:
+                # Estructura diferente (manejo del caso 'desconocido')
+                return {
+                    "temperature": "Desconocida",
+                    "wind_speed": "Desconocido",
+                    "description": "No disponible",
+                }
         except httpx.RequestError as e:
-            return {"temperature": "Desconocida", "wind_speed": "Desconocido", "description": f"Error: {e}"}
+            # Registrar el error en los logs del servidor
+            print(f"Error al obtener el clima para la ciudad {city}: {e}")
+            return {"temperature": "Desconocida", "wind_speed": "Desconocido", "description": "No disponible"}
 
-# Función para obtener `strCity` del evento
+async def get_weather_by_coordinates(coordinates: str, date: str) -> dict:
+   
+    latitude, longitude = convert_to_decimal(coordinates)
+
+    api_key = WEATHER_KEY
+    # url = f"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
+    url = f"http://api_clima:3000/clima?lat={latitude}&lon={longitude}"
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(url)
+            response.raise_for_status()
+            weather_data = response.json()
+            # Verificar si la estructura esperada está presente
+            if "main" in weather_data and "wind" in weather_data and "weather" in weather_data:
+                return {
+                    "temperature": weather_data["main"]["temp"],
+                    "wind_speed": weather_data["wind"]["speed"],
+                    "description": weather_data["weather"][0]["description"],
+                }
+            else:
+                # Estructura diferente (manejo del caso 'desconocido')
+                return {
+                    "temperature": "Desconocida",
+                    "wind_speed": "Desconocido",
+                    "description": "No disponible",
+                }
+        except httpx.RequestError as e:
+            # Registrar el error en los logs del servidor
+            print(f"Error al obtener el clima para la ciudad {city}: {e}")
+            return {"temperature": "Desconocida", "wind_speed": "Desconocido", "description": "No disponible"}
+
+
+# Función para obtener `strMap` del evento
 async def get_city_for_event(id_venue: str, country: str) -> str:
     sports_key = SPORTS_KEY
     api_url = f"https://www.thesportsdb.com/api/v1/json/{sports_key}/lookupvenue.php?id={id_venue}"
@@ -133,6 +175,30 @@ async def get_city_for_event(id_venue: str, country: str) -> str:
             print(f"Error al consultar el evento {id_venue}: {e}")
             return country 
 
+# Función para obtener `strLocation` del evento
+async def get_location_for_event(id_venue: str, country: str) -> str:
+    sports_key = SPORTS_KEY
+    api_url = f"https://www.thesportsdb.com/api/v1/json/{sports_key}/lookupteam.php?id={id_venue}"
+    async with httpx.AsyncClient() as client:
+        try:
+            print("Consultando evento:", id_venue)
+            response = await client.get(api_url)
+            response.raise_for_status()
+            event_data = response.json()
+
+            # Imprime la estructura completa de la respuesta
+            print(f"Respuesta del evento {id_venue}:", event_data)
+
+            if "teams" in event_data and event_data["teams"]:
+                city = event_data["teams"][0].get("strLocation", None)
+                print(f"Ciudad obtenida para el evento {id_venue}: {city}")
+                return city if city else country  # Si no hay location, usa country
+            return country  
+        except httpx.RequestError as e:
+            print(f"Error al consultar el evento {id_venue}: {e}")
+            return country
+
+
 # Función para obtener el pronóstico usando OpenAI
 async def get_match_prediction(home_team: str, away_team: str, date: str, weather: dict) -> str:
     tokens = 200
@@ -154,93 +220,6 @@ async def get_match_prediction(home_team: str, away_team: str, date: str, weathe
     except openai.OpenAIError as e:
         return f"Error al obtener el pronóstico: {str(e)}"
 
-# Función para obtener el clima
-async def get_weather(city: str, date: str) -> dict:
-    api_key = WEATHER_KEY
-    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-    
-    async with httpx.AsyncClient() as client:
-        try:
-            response = await client.get(url)
-            response.raise_for_status()
-            weather_data = response.json()
-            return {
-                "temperature": weather_data["main"]["temp"],
-                "wind_speed": weather_data["wind"]["speed"],
-                "description": weather_data["weather"][0]["description"],
-            }
-        except httpx.RequestError as e:
-            return {"temperature": "Desconocida", "wind_speed": "Desconocido", "description": f"Error: {e}"}
-
-# Define el endpoint
-# @router.get("/next", response_model=List[Event])
-async def get_next_events():
-    sports_key = SPORTS_KEY
-    api_url = f"https://www.thesportsdb.com/api/v1/json/{sports_key}/eventsnextleague.php?id=4335"
-
-    try:
-        async with httpx.AsyncClient() as client:
-            response = await client.get(api_url)
-            response.raise_for_status()
-
-        data = response.json()
-        events = data.get("events", [])
-        enriched_events = []
-
-        for event in events:
-            home_team = event.get("strHomeTeam")
-            away_team = event.get("strAwayTeam")
-            date_event = event.get("dateEvent")
-            id_event = event.get("idEvent")
-            country = event.get("strCountry")
-            venue = event.get("idVenue")
-
-           
-            # Obtener la ciudad del evento, buscando primero strMap
-            city_or_map = await get_city_for_event(venue, country)
-
-            if re.search(r"°|′|″", city_or_map):  # Detectar si la respuesta contiene coordenadas (strMap)
-                print(f"Usando coordenadas: {city_or_map}")
-                weather = await get_weather_by_coordinates(city_or_map, date_event)
-            else:
-                print(f"Usando ciudad o país: {city_or_map}")
-                weather = await get_weather(city_or_map, date_event) 
-
-            # Obtener el pronóstico
-            pronostico = await get_match_prediction(home_team, away_team, date_event, weather)
-
-            # Enriquecer el evento con clima y pronóstico
-            enriched_events.append(Event(
-                idEvent=id_event,
-                strEvent=event.get("strEvent"),
-                strHomeTeam=home_team,
-                strAwayTeam=away_team,
-                idHomeTeam=event.get("idHomeTeam"),
-                idAwayTeam=event.get("idAwayTeam"),
-                dateEvent=date_event,
-                strTime=event.get("strTime"),
-                strHomeTeamBadge=event.get("strHomeTeamBadge"),
-                strAwayTeamBadge=event.get("strAwayTeamBadge"),
-                idLeague=event.get("idLeague"),
-                idVenue=event.get("idVenue"),
-                strVenue=event.get("strVenue", "Desconocido"),
-                clima=weather,
-                pronostico=pronostico,
-            ))
-
-        return enriched_events
-
-    except httpx.RequestError as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Error al comunicarse con la API externa: {e}",
-        )
-    except Exception as e:
-        raise HTTPException(
-            status_code=500,
-            detail=f"Error inesperado: {e}",
-        )
-
 @router.post("/update-events")
 async def update_events(db: Session = Depends(get_db)):
     sports_key = SPORTS_KEY
@@ -258,14 +237,23 @@ async def update_events(db: Session = Depends(get_db)):
 
             data = response.json()
             events = data.get("events", [])
+            ban = 0 # bandera para determinar si buscar weather por country despues de que falla por location 
+
+            # Recolectar los idEvent actuales desde la respuesta del API
+            current_event_ids = []
 
             for event in events:
                 id_event = event.get("idEvent")
+                if not id_event:
+                    continue
                 home_team = event.get("strHomeTeam")
                 away_team = event.get("strAwayTeam")
                 date_event = event.get("dateEvent")
                 country = event.get("strCountry")
                 venue = event.get("idVenue")
+                if not venue:
+                    continue
+                current_event_ids.append(id_event)
 
                 # Obtener la ciudad o coordenadas
                 city_or_map = await get_city_for_event(venue, country)
@@ -273,8 +261,23 @@ async def update_events(db: Session = Depends(get_db)):
                     print(f"Usando coordenadas: {city_or_map}")
                     weather = await get_weather_by_coordinates(city_or_map, date_event)
                 else:
+                    if country == city_or_map: # Significa que no encontro Map, se busca por location del home_team
+                        city_or_map = await get_location_for_event(event.get("idHomeTeam"), country)
+                        if city_or_map != country: # Significa que encontro Location 
+                            ban = 1
+
                     print(f"Usando ciudad o país: {city_or_map}")
+                    if city_or_map == 'Brazil':
+                        city_or_map = 'Brasil'
+
                     weather = await get_weather(city_or_map, date_event)
+
+                    if weather["temperature"] == "Desconocida" and ban: # Se intenta buscar por pais
+                        ban = 0
+                        city_or_map = country
+                        if city_or_map == 'Brazil':
+                            city_or_map = 'Brasil'
+                        weather = await get_weather(city_or_map, date_event)
 
                 # Obtener el pronóstico
                 pronostico = await get_match_prediction(home_team, away_team, date_event, weather)
@@ -314,6 +317,13 @@ async def update_events(db: Session = Depends(get_db)):
                 db.commit()
 
                 all_enriched_events.append(enriched_event)
+
+            # Eliminar eventos que ya no están en la respuesta del API
+            stored_events = db.query(EventModel).filter_by(id_league=id_league).all()
+            for stored_event in stored_events:
+                if stored_event.id_event not in current_event_ids:
+                    db.delete(stored_event)
+            db.commit()
 
         except httpx.RequestError as e:
             raise HTTPException(
